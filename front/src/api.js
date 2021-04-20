25e10f0f2de30e9509e606980b25017bf5c1ae88
theamazingeagle <oryolman@gmail.com>
2021-04-20 18:18:21 +0300

    front: tabs

--- front/src/api.js
+++ front/src/api.js
@@ -1,26 +1,130 @@
 export class API {
-  baseURL = 'http://localhost:8080';
+    baseURL = 'http://localhost:8080';
 
-  getClients() {
-    return fetch(
-      this.baseURL + '/api/client/get', {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
-  updateClient(client) {
-    return fetch(
-      this.baseURL + '/api/client/update', {
-      method: 'PATCH',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(client)
-    });
-  }
-  deleteClient(id) {
-    return fetch(
-      this.baseURL + '/api/client/delete/' + id, {
-      method: 'DELETE',
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
+    getClients() {
+        return fetch(
+            this.baseURL + '/api/client/get', {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        });
+    }
+
+    createClient(client) {
+        return fetch(
+            this.baseURL + '/api/client/create', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(client)
+        });
+    }
+
+    getClient(id) {
+        return fetch(
+            this.baseURL + '/api/client/get/' + id, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        });
+    }
+
+    updateClient(client) {
+        return fetch(
+            this.baseURL + '/api/client/update', {
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(client)
+        });
+    }
+
+    deleteClient(id) {
+        return fetch(
+            this.baseURL + '/api/client/delete/' + id, {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' },
+        });
+    }
+
+    getServices() {
+        return fetch(
+            this.baseURL + '/api/service/get', {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        });
+    }
+
+    createService(service) {
+        return fetch(
+            this.baseURL + '/api/service/create', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(service)
+        });
+    }
+
+    getService(id) {
+        return fetch(
+            this.baseURL + '/api/service/get/' + id, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        });
+    }
+
+    updateService(service) {
+        return fetch(
+            this.baseURL + '/api/service/update', {
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(service)
+        });
+    }
+
+    deleteService(id) {
+        return fetch(
+            this.baseURL + '/api/service/delete/' + id, {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' },
+        });
+    }
+
+    getOrders() {
+        return fetch(
+            this.baseURL + '/api/order/get', {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        });
+    }
+
+    createOrder(order) {
+        return fetch(
+            this.baseURL + '/api/order/create', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(order)
+        });
+    }
+
+    getOrder(id) {
+        return fetch(
+            this.baseURL + '/api/order/get/' + id, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        });
+    }
+
+    updateOrder(order) {
+        return fetch(
+            this.baseURL + '/api/order/update', {
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(order)
+        });
+    }
+
+    deleteOrder(id) {
+        return fetch(
+            this.baseURL + '/api/order/delete/' + id, {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' },
+        });
+    }
+
 }
--- front/src/app.jsx
+++ front/src/app.jsx
@@ -6,9 +6,42 @@ import { API } from './api.js';
 
 const api = new API();
 
+const TabContent = ({ title, element }) => (
+  <div className="tabcontent">
+    <h3>{title}</h3>
+    <div>{element}</div>
+  </div>
+);
+
+function Tabs({ items }) {
+  const [active, setActive] = React.useState(null);
+
+  const openTab = e => setActive(+e.target.dataset.index);
+
+  return (
+    <div>
+      <div className="tab">
+        {items.map((n, i) => (
+          <button
+            onClick={openTab}
+            data-index={i}
+          >{n.title}</button>
+        ))}
+      </div>
+      {items[active] && <TabContent {...items[active]} />}
+    </div>
+  );
+}
+
+const items = [
+  { title: "Список клиентов", element: React.createElement(ClientsList) },
+  { title: "Список услуг", element: 'Paris' },
+  { title: "Список заказов", element: 'Tokyo' },
+];
+
 ReactDOM.render(
-    <AppContext.Provider value={{ api }}>
-        <ClientsList />
-    </AppContext.Provider>,
-    document.getElementById("app")
+  <AppContext.Provider value={{ api }}>
+    <Tabs items={items} />
+  </AppContext.Provider>,
+  document.getElementById("app")
 )
